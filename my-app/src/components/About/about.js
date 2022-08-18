9d3bed2050371ace88dfd90723e6a67bc467544f
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-08-18 12:18:13 -0500

    updated caoursel and added copy

--- my-app/src/components/About/about.js
+++ my-app/src/components/About/about.js
@@ -17,7 +17,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CardHeader from '@mui/material/CardHeader';
 import { useState } from 'react';
 //import animation framework 
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 
 //MUI Icons
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -35,17 +35,18 @@ const tiers = [
     {
         icon: <AccountCircleIcon fontSize='large'/>,
         title: 'Patient',
-        subheader: 'Value Statement',
-        description: [
+        subheader: 'At DigiBeat, Your health is our priority. Many Americans often struggle to find time availability or funds necessary for frequent trips to a physician, especially for long term or recurring diseases. Potentially even more concerning is the fear of potentially missing critical indicators for the start or progression of illness. Our cutting edge data collection and analysis detects abnormalities in your health profile early and with exceptional accuracy.',
+        buttonText: 'Sign up for free',
+        description:[
+
 
         ],
-        buttonText: 'Sign up for free',
         buttonVariant: 'outlined',
     },
     {
         icon: <LocalHospitalIcon  fontSize='large'/>,
         title: 'Physician',
-        subheader: 'Value Statement',
+        subheader: 'DigiBeat provides an unprecedented connection not only between patients and their own health, but also with their primary care physician. For a physician, time and efficiency are vital aspects of a successful practice. Our device is designed to streamline the time between auscultation and diagnosis with simple to follow guided exams, intricate health insights, and an easily manageable patient dashboard platform. Monitor disease progression of each patient in real time with personalized health trends, and create a care management plan with the assistance of AI analyzed feedback.',
         description: [
 
         ],
@@ -55,7 +56,7 @@ const tiers = [
     {   
         icon: <PaidIcon  fontSize='large'/>,
         title: 'Payer',
-        subheader: 'Value Statement',
+        subheader: 'As of 2022, 6.2 million adults in the U.S. have heart failure. The elevated ER admission rates as a result of this figure generate an average cost of $15 billion per year for Insurance payers. DigiBeat’s Clinical solution allows physicians to detect and monitor disease progression sooner and more effectively, ultimately reducing the number of chronic stage heart failure admissions and readmissions. We aim to provide payers the tools they need to remain competitive in a capitated system moving towards value based care.',
         description: [
 
         ],
@@ -88,12 +89,12 @@ export default function About() {
                             variant="h2"
                             align="center"
                             color="text.primary"
-                            
+                            p='20px'
                         >
                             What We Do
                         </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                            Mission Statement
+                        <Typography fontSize='18px' align="center" color="text.secondary">
+                        At DigiBeat, we have created an innovative Digital Stethoscope and medical exam system which combines traditional methods of medical practice with current technologies in artificial intelligence and augmented reality to improve patient outcomes. By bridging together the diagnostic power of AI with the guidance capabilities of augmented reality, we have found a way to bring the auscultation examination process into the home. Opening up vast opportunities to better understand patient outcomes, medication efficacies, disease progressions, and health expectancies. All of which will help to reduce payer costs.
                         </Typography>
                         <Stack
                             sx={{ pt: 4 }}
@@ -120,21 +121,19 @@ export default function About() {
                             >
                                 Who We Help
                             </Typography>
-                            <Typography variant="h5" align="center" color="text.secondary" component="p">
-                                Value statement
-                            </Typography>
+                            {/* <Typography variant="h5" align="center" color="text.secondary" component="p"></Typography> */}
                         </Container>
                         {/* End hero unit */}
-                        <Container maxWidth="md" component="main">
-                            <Grid container spacing={5} alignItems="flex-end">
+                        <Container maxWidth="md" component="main" class='row'>
+                            <Grid container alignItems="flex-start" justifyContent='space-evenly'>
                                 {tiers.map((tier) => (
                                     // Enterprise card is full width at sm breakpoint
                                     <Grid
                                         item
                                         key={tier.title}
-                                        xs={12}
-                                        sm={tier.title === 'Enterprise' ? 12 : 6}
-                                        md={4}
+                                        class='col-lg-3'
+                                        padding='5px'
+                                        marginX='10px'
                                     >
                                         <Card>
                                         <CardMedia component="h2" variant="h3" color="text.primary" align='center'>
@@ -159,10 +158,7 @@ export default function About() {
                                             <CardContent>
                                                 <Box
                                                     sx={{
-                                                        display: 'flex',
-                                                        justifyContent: 'center',
-                                                        alignItems: 'baseline',
-                                                        mb: 2,
+                                                       p: "auto"
                                                     }}
                                                 >
                                                     
@@ -191,7 +187,7 @@ export default function About() {
                     {/* End hero unit */}
                     {/* Start the team page */}
                     <Typography variant="h2" align="center">Meet The Team</Typography>
-                    <Grid>
+                    <Grid container alignItems="flex-start" justifyContent='space-evenly'>
                             <Grid class='row'>
                                 <Card  class='col-lg-4'>
                                     <CardMedia
@@ -213,11 +209,11 @@ export default function About() {
                                            Winston T. Richards, MD
                                         </Typography>
                                         <Typography>CMO</Typography>
-                                        {/* {isHovering && ( */}
+                                        {isHovering && (
                                         <Typography className='win2'>
                                         Trauma Surgeon located in the North Central Florida area with a background in medicine and applied mathematics. As the founder and CEO, Winston has seen through the development of the Digibeat Electronic Stethoscope from its original prototype developed in Winston’s garage.
                                         </Typography>
-                                        {/* )} */}
+                                         )} 
                                     </CardContent>
                                 </Card>
                                 <Card class='col-lg-4' className='cliff'>
@@ -230,19 +226,19 @@ export default function About() {
                                         image={cliff}
                                         alt="random"
                                         className='cliff1'
-                                        // onMouseEnter={() => setIsHovering(true)}
-                                        // onMouseLeave={() => setIsHovering(false)}
+                                        onMouseEnter={() => setIsHovering(true)}
+                                        onMouseLeave={() => setIsHovering(false)}
                                     />
                                     <CardContent sx={{ flexGrow: 1 }}>
                                         <Typography gutterBottom variant="h5" component="h2">
                                           Clifford Steele
                                         </Typography>
                                         <Typography>CEO</Typography>
-                                        {/* {isHovering && ( */}
+                                        {isHovering && (
                                         <Typography className='cliff2'>
                                         Cliff has a background in Aeronautical, Aerospace and Astronautical Engineering from Embry-Riddle Aeronautical University. He focuses on project organization while being the brains behind the engineering that goes into developing the Digibeat Electronic Stethoscope.
                                         </Typography>
-                                        {/* )} */}
+                                        )}
                                     </CardContent>
                                 </Card>
                                 <Card class='col-lg-4' className='karl'>
@@ -258,10 +254,8 @@ export default function About() {
                                         image={karll}
                                         alt="random"
                                         className='karl1'
-
-
-                                        // onMouseEnter={() => setIsHovering(true)}
-                                        // onMouseLeave={() => setIsHovering(false)}
+                                        onMouseEnter={() => setIsHovering(true)}
+                                        onMouseLeave={() => setIsHovering(false)}
                                     />
                                     <CardContent sx={{ flexGrow: 1 }}>
                                         <Typography gutterBottom variant="h5" component="h2" 
@@ -270,11 +264,11 @@ export default function About() {
                                           Karl Dockendorf
                                         </Typography>
                                         <Typography>CTO</Typography>
-                                        {/* {isHovering && ( */}
+                                        {isHovering && (
                                         <Typography className='karl2'>
                                         Karl earned his BS and ME in electrical and computer engineering followed by a Ph.D. in biomedical engineering (neural engineering focus) from the University of Florida.  Karl is well versed in the design of computer hardware, intelligent information processing, machine learning, and adaptation of neuronal networks.  
                                         </Typography>
-                                        {/* )} */}
+                                        )}
                                     </CardContent>
                                 </Card>
                                 <Card className='bottomEmployees'>
