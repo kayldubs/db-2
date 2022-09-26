754210e9617d940311ab309326ba3d821cf55a61
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-09-26 16:43:39 -0500

    more queries

--- my-app/src/components/Team4/Team4.js
+++ my-app/src/components/Team4/Team4.js
@@ -25,7 +25,7 @@ function Featured() {
         setIsHovering(false);
     }
   return (
-    <Grid container spacing={5} rowGap={{xl: 1}} className='employeeGrid' style={{
+    <Grid container spacing={1} rowGap={{xl: 12}} className='employeeGrid' style={{
         display: 'flex',
         position:'relative',
         justifyContent: 'center',
